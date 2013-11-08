3803def5d9da08fb462b72a3a2fc5a48b737186a
jansoe <j.soelter@fu-berlin.de>
2013-11-08 17:43:55 +0100

    refactored more object oriented

--- datamaker.py
+++ datamaker.py
@@ -10,11 +10,16 @@ from scipy.spatial.distance import squareform
 
 
 def gaussian_influence(mu, width):
-    ''' creates a 2D-function of gaussian influence sphere'''
+    ''' creates a 2D-function for gaussian influence sphere'''
     return lambda x, y: np.exp(-width * ((x - mu[0]) ** 2 + (y - mu[1]) ** 2))
 
 def correlated_samples(cov, num_sampl, marginal_dist):
-    ''' creates correlated samples with a gaussian copula '''
+    ''' creates correlated samples with a gaussian copula 
+        
+        cov: covariance matrix (copula)
+        num_sampl: numbers of sample to be drawn
+        marginal_dist: marginal distribution samples are drawn from
+    '''
 
     # Create Gaussian Copula
     dependence = np.random.multivariate_normal([0] * cov.shape[0], cov, num_sampl)
@@ -25,10 +30,10 @@ def correlated_samples(cov, num_sampl, marginal_dist):
     dependend_samples = marginal_dist.ppf(uniform_dependence)
     return dependend_samples
 
-
 def group_covmtx(rho_intra, rho_inter, num_groups, num_objects):
     ''' create a covarince matrix with groups
     
+    create covariance matrix for num_groups*num_objects variables 
     in each group are num_objects with a covariance of rho_intra. 
     objects between groups have a covariance of rho_intra 
     '''
@@ -38,7 +43,6 @@ def group_covmtx(rho_intra, rho_inter, num_groups, num_objects):
 
     cov = rho_inter * np.ones((num_groups * num_objects, num_groups * num_objects))
     for group_num in range(num_groups):
-        print group_num * num_objects, (group_num + 1) * num_objects
         cov[group_num * num_objects:(group_num + 1) * num_objects,
             group_num * num_objects:(group_num + 1) * num_objects] = intra_cov
     return cov
@@ -52,11 +56,32 @@ def adjusted_gamma(mean, var):
         print '!!! Warning !!! - shape parameter: ', str(shape)
     return gamma(shape, scale=scale)
 
+def crosscor(a1, a2):
+    '''calculate crosscorrelation between two matrices'''
+
+    num_var = a1.shape[0]
+    return np.corrcoef(np.vstack((a1, a2)))[num_var:, :num_var]
 
 class Dataset():
+    """Surrogate dataset of sources and their mixed observations
+    
+    Keyword arguments: 
+    param : dictionary of parameters
+            shape: tuple with spatial extent of observed area in pixel
+            gridpoints: number of sources in one dimension
+            width: width of spatial influence
+            latents: number of sources
+            covgroups: number of correlated source groups
+            cov: correlation of activation within a source group
+            mean: mean source activation
+            var: source activation variance
+            no_samples: number of independent observations (stimuli)
+            act_time: model time cours of activation
+            noisevar: sigma of gaussian pixel noise
+    """
+
 
     def __init__(self, param):
-        ''' creates sources and their mixed observations '''
 
         # create spatial sources
         num_grid = param.get('gridpoints', 9)
@@ -73,7 +98,8 @@ class Dataset():
         covgroups = param.get('covgroups', 4)
         self.cov = group_covmtx(param['cov'], 0.1, covgroups, param['latents'] / covgroups)
         marginal_dist = adjusted_gamma(param['mean'], param['var'])
-        self.activ_pre = correlated_samples(self.cov, param['no_samples'], marginal_dist).T
+        self.activ_pre = correlated_samples(self.cov, param['no_samples'],
+                                             marginal_dist).T
         self.activ_pre[np.isnan(self.activ_pre)] = 0
         # fold with single stim timecourse
         if param['act_time']:
@@ -84,3 +110,68 @@ class Dataset():
         # add noise
         noise = param['noisevar'] * np.random.randn(*self.observed_raw.shape)
         self.observed = self.observed_raw.copy() + noise
+
+    def cor2source(self, estimator):
+        """match sources to their best estimate and calc correlation
+        
+        each source is matched to the estimator it's exhibits the highest spatial
+        correlation
+        
+        Parameters
+        ----------
+        estimator: ImageAnalysisComponents.TimeSeries
+        
+        Returns
+        ------- 
+        matchid: numpy.array
+            i-th entry contains index of estimator matched to i-th source
+        st_cor: numpy.array
+            i-th entry contains temporal correlation of estimator for i-th source
+        sp_cor: numpy.array
+            i-th entry contains spatial correlation of estimator for i-th source
+        """
+
+        # temporal corellation with all sources
+        tmp_cor = crosscor(self.activation.T, estimator._series.T)
+        # spatial correlations with all sources
+        sp_cor = crosscor(self.spt_sources, estimator.base._series)
+        matchid = np.nanargmax(np.abs(sp_cor), 0)
+
+        # temporal correlation at best spatial corralation
+        st_cor = np.abs(tmp_cor[matchid, range(self.spt_sources.shape[0])])
+        return matchid, st_cor, sp_cor
+
+    def mse2source(self, estimator, local=0):
+        """match sources to their best estimate and calc mean squared error (MSE)
+        
+        each source is matched to the estimator it's exhibits the lowest MSE
+        
+        Parameters
+        ----------
+        estimator: ImageAnalysisComponents.TimeSeries
+        
+        Returns
+        ------- 
+        mse: numpy.array
+            i-th entry contains MSE of estimator matched to i-th source
+        """
+
+        best_mse = []
+        for source_ind in range(self.activation.shape[1]):
+
+            mf_pixelpart = estimator.base._series
+            data_pixelpart = self.spt_sources[source_ind]
+            if local:
+                mask = self.spt_sources[source_ind] > local
+                mf_pixelpart = mf_pixelpart[:, mask]
+                data_pixelpart = data_pixelpart[mask]
+
+            source = np.outer(self.activation[:, source_ind], data_pixelpart)
+            source_norm = np.linalg.norm(source)
+            mse_components = []
+            for e_ind in range(estimator.num_objects):
+                estimate = np.outer(estimator._series[:, e_ind], mf_pixelpart[e_ind])
+                mse = np.linalg.norm(source - estimate) / source_norm
+                mse_components.append(mse)
+            best_mse.append(np.min(mse_components))
+        return best_mse
